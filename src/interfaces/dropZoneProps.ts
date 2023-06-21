bae831873cd83b53c5a2bcc40a1e49090b902a5c
zael-mab <zackliwa@gmail.com>
2023-06-21 21:13:02 +0100

    fix typos

--- src/interfaces/dropZoneProps.ts
+++ src/interfaces/dropZoneProps.ts
@@ -1,11 +1,13 @@
 
 interface DropZone {
+  zoneIndex: number;
   items: string[];
   text: string;
   handleAddText: () => void;
   addText: (e: React.ChangeEvent<HTMLInputElement>) => void;
   handleClear: () => void;
   newItem: (item: string) => void;
+  removeItem: (index: number) => void;
 }
 
 export interface DropZoneProps {
